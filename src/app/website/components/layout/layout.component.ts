c9ca225c248fd749bcab1155fd0072927747836b
luisgv94 <lgomero94@gmail.com>
2023-03-26 18:08:30 -0500

    cambios en layout

--- src/app/website/components/layout/layout.component.ts
+++ src/app/website/components/layout/layout.component.ts
@@ -3,13 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.scss']
+  styleUrls: ['./layout.component.scss'],
 })
-export class LayoutComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class LayoutComponent {
+  constructor() {}
 }
